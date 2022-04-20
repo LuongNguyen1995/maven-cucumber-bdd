0195f37041d0e16c6d918b8245a0a70fbe8bbd4a
LuongNguyen1995 <luong.epu.dtvt1@gmail.com>
2022-04-20 22:09:12 +0700

    Update Cucumber Parameter and Cucumber Option

--- src/test/java/features/verifyFacebook.feature
+++ src/test/java/features/verifyFacebook.feature
@@ -1,8 +1,48 @@
+@login
 Feature: Facebook login page
 
-  @displayed
-  Scenario: Verify login page
+  @no_param
+  Scenario: Scenario have no parameter
     Given Open facebook application
-    Then Verify email textbox is displayed
-    And Verify password textbox is displayed
+    When Input to Username textbox 
+    And Input to Password textbox 
     And Close application
+    
+  @param_mark
+  Scenario: Scenario have no parameter
+    Given Open facebook application
+    When Input to Username textbox with "automationfc@gmail.com"
+    And Input to Password textbox with "123123"
+    And Click to Submit button
+    And Close application
+    
+  @param_no_mark
+  Scenario: Scenario have no parameter
+    Given Open facebook application
+    When Input to Username textbox with automationfc@gmail.com
+    And Input to Password textbox with 123123
+    And Click to Submit button
+    And Close application
+    
+  @mutiple_param
+  Scenario: Scenario have parameter
+    Given Open facebook application
+    When Input to Username textbox with "automationfc@gmail.com" and Password with "123123"
+    And Click to Submit button
+    And Close application
+    
+  @datatable_step
+  Scenario Outline: Create new Customer with email <Username>
+    Given Open facebook application
+     When Input to Username and Password
+      | Username   | Password   | Address   | 
+      | <Username> | <Password> | <Address> | 
+      And Click to Submit button
+     Then Verify submitted infor displayed
+      | Username   | Address   | 
+      | <Username> | <Address> | 
+      And Close application
+  
+    Examples: 
+      | Username               | Password | City     | Address | Phone      | 
+      | automationfc@gmail.com | 123123   | Bac Ninh | Tu Son  | 0999111999 | 
--- src/test/java/stepDefinitions/FacebookSteps.java
+++ src/test/java/stepDefinitions/FacebookSteps.java
@@ -2,13 +2,15 @@ package stepDefinitions;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import cucumber.api.DataTable;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FacebookSteps {
@@ -21,20 +23,78 @@ public class FacebookSteps {
 	    driver.get("https://www.facebook.com/");
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
-
-	@Then("^Verify email textbox is displayed$")
-	public void verifyEmailTextboxIsDisplayed()  {
-		Assert.assertTrue(driver.findElement(By.id("email")).isDisplayed());
-	    
+	
+	@When("^Input to Username textbox$")
+	public void inputToUsernameTextbox() {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("automationfc@gmail.com");
 	}
 
-	@Then("^Verify password textbox is displayed$")
-	public void verifyPasswordTextboxIsDisplayed()  {
-		Assert.assertTrue(driver.findElement(By.id("pass")).isDisplayed());
-	    
+	@When("^Input to Password textbox$")
+	public void inputToPasswordTextbox() {
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys("123123");
 	}
 	
-	@Then("^Close application$")
+	@When("^Input to Username textbox with \"([^\"]*)\"$")
+	public void inputToUsernameTextboxWith(String email) {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(email);
+		
+	}
+	
+	@When("^Input to Password textbox with \"([^\"]*)\"$")
+	public void inputToPasswordTextboxWith(String password)  {
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
+	
+
+	@When("^Input to Username textbox with ([^\"]*)$")
+	public void inputToUsernameTextbox(String email) {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(email);
+		
+	}
+
+	@When("^Input to Password textbox ([^\"]*)$")
+	public void inputToPasswordTextbox(String password) {
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
+	
+	@When("^Input to Username textbox with \"([^\"]*)\" and Password with \"([^\"]*)\"$")
+	public void inputToUsernameTextboxWithAndPasswordWith(String email, String password)  {
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(email);
+		
+		driver.findElement(By.id("pass")).clear();
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
+
+	@When("^Input to Username and Password$")
+	public void inputToUsernameAndPassword(DataTable arg1)  {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	}
+	
+	@Then("^Verify submitted infor displayed$")
+	public void verifySubmittedInforDisplayed(DataTable arg1)  {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	}
+
+	@When("^Click to Submit button$")
+	public void clickToSubmitButton() {
+		driver.findElement(By.name("login")).click();
+	}
+
+	
+	@And("^Close application$")
 	public void closeApplication()  {
 	   driver.quit();
 	}
